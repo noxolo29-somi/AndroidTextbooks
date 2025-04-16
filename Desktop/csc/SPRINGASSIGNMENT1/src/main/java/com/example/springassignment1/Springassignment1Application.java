435a55a5c106fd3557801fa3350a9a18244a8ff1
T-Khumalo <165251621+T-Khumalo@users.noreply.github.com>
2025-04-16 08:52:40 +0200

    first commit

---
+++ Desktop/csc/SPRINGASSIGNMENT1/src/main/java/com/example/springassignment1/Springassignment1Application.java
@@ -0,0 +1,13 @@
+package com.example.springassignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springassignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springassignment1Application.class, args);
+    }
+
+}
